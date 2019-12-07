dd208460d7d99b476b4df84c5f320904cfd74173
lee-sj <201302386@hufs.ac.kr>
2019-12-07 17:27:28 +0900

    fix

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
-<?php header('Location: /index.html'); 
+<?php header('Location: /index.html');?>
 
-    if (!(preg_match('/(iPhone|Android|Opera Mini|SymblanOS|Windows CE|BlackBerry|Nokia|SonyEricsson|Web OS|PalmOS)/i',$_SERVER['HTTP_USER_AGENT']))){
-        include "sorry_pc.html"
+<?
+  if (!(preg_match('/(iPhone|Android|Opera Mini|SymblanOS|Windows CE|BlackBerry|Nokia|SonyEricsson|Web OS|PalmOS)/i',$_SERVER['HTTP_USER_AGENT']))){
+    include "sorry_pc.html";
     }
-
-?>
-
+ ?>
+  
